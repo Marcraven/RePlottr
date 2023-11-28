18c859353b18a2c294e37bd710a4f5fe154f63f5
marcelonora <marcenora0@gmail.com>
2023-11-28 16:58:39 +0100

    yolo update

--- ObjectRecognition/train_yolo.py
+++ ObjectRecognition/train_yolo.py
@@ -8,19 +8,21 @@ from ultralytics import YOLO
 
 def train_model():
     # Load the pre-trained model
-    model = YOLO("yolov8s-p2.yaml").load("yolov8s.pt")
+    model = YOLO("yolov8s-p2.yaml").load("ObjectRecognition/yolov8s.pt")
     currentdir = os.path.dirname(os.path.abspath(__file__))
+    pathdir = "ObjectRecognition/yolo/dataset/results"
     # Train the model
     model.train(
         data=currentdir + "/dataset.yaml",
-        epochs=200,
+        epochs=10,
         imgsz=320,
         save=True,
         format="onnx",
     )  # Set imgsz to 320 for training on 320xsomething images
 
     # Export the model to ONNX format
-    path = model.export()
+    export_path = pathdir + "/my_trained_model.onnx"
+    path = model.export(export_path)
     print(path)
 
 
