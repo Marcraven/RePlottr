c9380032c10d527b37a6ed6b23b30188d4320789
Marc Raventos <marcraven@gmail.com>
2023-12-07 15:21:58 +0100

    benchmark in progress

--- donutplot/interface/benchmark.py
+++ donutplot/interface/benchmark.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 import json
 
-file_path = TEST_PATH + "metadata.jsonl"
+file_path = os.path.join(TEST_PATH, "metadata.jsonl")
 
 # Open the JSONL file for reading
 gt = []
@@ -36,46 +36,50 @@ precision_list = np.ndarray(len(gt))
 recall_list = np.ndarray(len(gt))
 error_list = np.ndarray(len(gt))
 
-
+failed_ones = 0
 for i in range(len(gt)):
-    result = make_prediction(TEST_PATH + str(i).zfill(4) + ".jpg")
-    result_df = series_to_df(result["data_dicts"])
-    gt_df = series_to_df(gt[i]["ground_truth"]["data_dicts"])
+    result = make_prediction(os.path.join(TEST_PATH, str(i).zfill(4) + ".jpg"))
+    if result == "failed":
+        failed_ones += 1
+    else:
+        result_df = series_to_df(result["data_dicts"])
+        gt_df = series_to_df(gt[i]["ground_truth"]["data_dicts"])
 
-    neigh = NearestNeighbors(n_neighbors=1)
-    scaler = MinMaxScaler()
+        neigh = NearestNeighbors(n_neighbors=1)
+        scaler = MinMaxScaler()
 
-    gt_df.iloc[:, 1:] = scaler.fit_transform(X=gt_df.iloc[:, 1:])
+        gt_df.iloc[:, 1:] = scaler.fit_transform(X=gt_df.iloc[:, 1:])
 
-    result_df.iloc[:, 1:] = scaler.transform(result_df.iloc[:, 1:])
+        result_df.iloc[:, 1:] = scaler.transform(result_df.iloc[:, 1:])
 
-    neigh.fit(gt_df.iloc[:, 1:])
+        neigh.fit(gt_df.iloc[:, 1:])
 
-    distances, indices = neigh.kneighbors(
-        result_df.iloc[:, 1:], 1, return_distance=True
-    )
+        distances, indices = neigh.kneighbors(
+            result_df.iloc[:, 1:], 1, return_distance=True
+        )
 
-    knnd_result = pd.concat(
-        [result_df, pd.Series(indices.flatten()), pd.Series(distances.flatten())],
-        axis=1,
-    ).sort_values(by=0)
+        knnd_result = pd.concat(
+            [result_df, pd.Series(indices.flatten()), pd.Series(distances.flatten())],
+            axis=1,
+        ).sort_values(by=0)
 
-    original_points = gt_df.shape[0]
-    true_positives = len(knnd_result.iloc[:, 3].unique())
-    false_positives = len(knnd_result.iloc[:, 3]) - true_positives
+        original_points = gt_df.shape[0]
+        true_positives = len(knnd_result.iloc[:, 3].unique())
+        false_positives = len(knnd_result.iloc[:, 3]) - true_positives
 
-    correctly_assigned = np.array(knnd_result.iloc[:, 0]) == np.array(
-        gt_df.iloc[knnd_result.iloc[:, 3], 0]
-    )
+        correctly_assigned = np.array(knnd_result.iloc[:, 0]) == np.array(
+            gt_df.iloc[knnd_result.iloc[:, 3], 0]
+        )
 
-    precision = round(sum(correctly_assigned) * 100 / len(correctly_assigned), 2)
-    recall = round(true_positives * 100 / original_points, 2)
-    error = round(np.mean(distances) * 100, 2)
+        precision = round(sum(correctly_assigned) * 100 / len(correctly_assigned), 2)
+        recall = round(true_positives * 100 / original_points, 2)
+        error = round(np.mean(distances) * 100, 2)
 
-    precision_list[i] = precision
-    recall_list[i] = recall
-    error_list[i] = error
+        precision_list[i] = precision
+        recall_list[i] = recall
+        error_list[i] = error
 
+failed_fraction = failed_ones / len(gt)
 precision_mean = np.mean(precision_list)
 recall_mean = np.mean(recall_list)
 error_mean = np.mean(error_list)
